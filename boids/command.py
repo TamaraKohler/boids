e80140a8d5b6add8c9ee916c897453e494323ac6
Tamara Kohler <tamara.kohler.16@ucl.ac.uk>
2017-02-23 20:32:05 +0000

    Corrected command.py to correctly read data from the configuration file

--- boids/command.py
+++ boids/command.py
@@ -8,11 +8,11 @@ from .Boids import Boids
 #from pkg_resources import resource_filename, resource_exists, Requirement
 
 # Extract information from the config file 
-#with open(os.path.join(os.path.abspath(__file__),'config.yaml')) as config_file:
- #   config=yaml.load(config_file)
- #   globals().update(config)
-#input_position_limits = np.array([minimum_position,maximum_position])
-#input_velocity_limits = np.array([minimum_velocity,maximum_velocity])
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),'config.yaml')) as config_file:
+    config=yaml.load(config_file)
+    globals().update(config)
+input_position_limits = np.array([minimum_position,maximum_position])
+input_velocity_limits = np.array([minimum_velocity,maximum_velocity])
 
 
 # Input parameters of the simulation
@@ -21,11 +21,9 @@ y_axes_limits = [-500,1500]
 number_of_frames = 50
 number_of_intervals = 50
 
-# Create an instance of the boids’ class with information from configuration file
-#boids = Boids(input_position_limits, input_velocity_limits, input_boid_count, input_move_to_middle_strength, input_alert_distance, input_formation_flying_distance, input_formation_flying_strength)
+#Create an instance of the boids’ class with information from configuration file
+boids = Boids(input_position_limits, input_velocity_limits, input_boid_count, input_move_to_middle_strength, input_alert_distance, input_formation_flying_distance, input_formation_flying_strength)
 
-# Create an instance of the boids’ class with default parameters
-boids = Boids()
 
 # Animate the boids
 figure=plt.figure()
